9b54f2609017a739ac04cc51626b534bd6f94adf
Mura <muramarch@gmail.com>
2023-03-11 20:26:20 +0600

    Added registr, models and admin

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from personal.models import Subject, Annual, Grade, PersonalFile
+
+admin.site.register(Subject)
+admin.site.register(Annual)
+admin.site.register(Grade)
+admin.site.register(PersonalFile)
