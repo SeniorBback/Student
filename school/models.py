8e4ce48cfa6eb25fd5b79a0ea09413930b44ad09
JustSultan <triple.ratatui@gmail.com>
2023-03-11 18:22:01 +0600

    добавлены вербосы

--- school/models.py
+++ school/models.py
@@ -4,7 +4,7 @@ from online_queue.models import Student
 
 
 class Region(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='имя')
 
     class Meta:
         verbose_name = 'Область'
@@ -14,7 +14,7 @@ class Region(models.Model):
 
 
 class City(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='город')
 
     class Meta:
         verbose_name = 'Город'
@@ -25,7 +25,7 @@ class City(models.Model):
 
 
 class District(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='район')
 
     class Meta:
         verbose_name = 'Район'
@@ -36,10 +36,10 @@ class District(models.Model):
 
 
 class School(models.Model):
-    fullname = models.CharField(max_length=255)
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
-    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+    fullname = models.CharField(max_length=255, verbose_name='полное имя')
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True, verbose_name='регион')
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, verbose_name='город')
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, verbose_name='район')
 
     class Meta:
         verbose_name = 'Школа'
@@ -47,10 +47,10 @@ class School(models.Model):
 
 
 class GoldenCertificate(models.Model):
-    student_fullname = models.OneToOneField(Student, max_length=50, on_delete=models.SET_NULL)
-    school_fullname = models.ForeignKey(School, max_length=255, on_delete=models.SET_NULL, null=True)
-    points = models.IntegerField()
-    year = models.IntegerField()
+    student_fullname = models.OneToOneField(Student, max_length=50, on_delete=models.SET_NULL, verbose_name='имя студента')
+    school_fullname = models.ForeignKey(School, max_length=255, on_delete=models.SET_NULL, null=True, verbose_name='название школы')
+    points = models.IntegerField(verbose_name='оценки')
+    year = models.IntegerField(verbose_name='год')
 
     class Meta:
         verbose_name = 'Золотой сертификат'
@@ -58,10 +58,10 @@ class GoldenCertificate(models.Model):
 
 
 class Olympiad(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, max_length=255)
-    school = models.ForeignKey(School, max_length=255, on_delete=models.SET_NULL, null=True)
-    points = models.IntegerField()
-    year = models.IntegerField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, max_length=255, verbose_name='студент')
+    school = models.ForeignKey(School, max_length=255, on_delete=models.SET_NULL, null=True, verbose_name='школа')
+    points = models.IntegerField(verbose_name='оценки')
+    year = models.IntegerField(verbose_name='год')
 
     class Meta:
         verbose_name = 'Олимпиада'
