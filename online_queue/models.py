98dd340863e99d1dcd6603a83624490c99500b94
Osmon23 <oma_23_03_98@mail.ru>
2023-03-11 17:12:30 +0600

    add model new

--- online_queue/models.py
+++ online_queue/models.py
@@ -1,22 +1,42 @@
 from django.db import models
+from django.utils import timezone
 
 
 class StudentPhoto(models.Model):
-    photo = models.ImageField(upload_to='student/')
+    photo = models.ImageField(upload_to='student/', verbose_name='фото')
 
 
 class Student(models.Model):
-    fullname = models.CharField(max_length=255)
-    pin = models.CharField(max_length=255)
-    birthdate = models.DateField()
-    photo = models.ForeignKey(StudentPhoto, on_delete=models.SET_NULL)
-    school = models.ForeignKey(School, on_delete=models.SET_NULL)
-    grade = models.IntegerField()
-    father = models.CharField(max_length=255)
-    mother = models.CharField(max_length=255)
+    fullname = models.CharField(max_length=255, verbose_name='фио')
+    pin = models.CharField(max_length=255, verbose_name='пин')
+    birthdate = models.DateField(verbose_name='дата_рождения')
+    photo = models.ForeignKey(StudentPhoto, on_delete=models.SET_NULL, verbose_name='фото')
+    school = models.ForeignKey(School, on_delete=models.SET_NULL, verbose_name='школа')
+    grade = models.IntegerField(verbose_name='класс')
+    father = models.CharField(max_length=255, verbose_name='отец')
+    mother = models.CharField(max_length=255, verbose_name='мать')
 
 
 class OnlineQueue(models.Model):
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    sms = models.TextField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE, verbose_name='школа')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='студент')
+    sms = models.TextField(verbose_name='смс')
+
+
+class NewPhoto(models.Model):
+    photo = models.ImageField(upload_to='new_photo/', verbose_name='фото')
+
+    class Meta:
+        verbose_name = 'Новое фото'
+        verbose_name_plural = 'Новые фото'
+
+
+class New(models.Model):
+    title = models.CharField(max_length=255, verbose_name='заголовок')
+    text = models.CharField(max_length=255, verbose_name='текст')
+    photo = models.ForeignKey(NewPhoto, on_delete=models.SET_NULL, verbose_name='фото')
+    date = models.DateTimeField(verbose_name='дата', default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
