53441e1a1f404c29b6a40df94e05b5c639ddce78
Mura <muramarch@gmail.com>
2023-03-12 06:36:51 +0600

    updated personal

--- personal/views.py
+++ personal/views.py
@@ -1,1 +1,52 @@
 from django.shortcuts import render
+from personal.serializers import SubjectlSerializer, AnnualSerializer, GradeSerializer, PersonalFileSerializer
+
+from personal.models import Subject, Annual, Grade, PersonalFile
+
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+class SubjectView(ModelViewSet):
+    serializer_class = SubjectlSerializer
+    queryset = Subject.objects.all()
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['name']
+    filterset_fields = ['name']
+
+
+class AnnualView(ModelViewSet):
+    serializer_class = AnnualSerializer
+    queryset = Annual.objects.all()
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['subject', 'quarter_1', 'quarter_2', 'quarter_3', 'quarter_4', 'quarter_final']
+    ordering_fields = ['subject', 'quarter_1', 'quarter_2', 'quarter_3', 'quarter_4', 'quarter_final']
+    filterset_fields = ['subject', 'quarter_1', 'quarter_2', 'quarter_3', 'quarter_4', 'quarter_final']
+
+
+
+class GradeView(ModelViewSet):
+    serializer_class = GradeSerializer
+    queryset = Annual.objects.all()
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['grade']
+    ordering_fields = ['grade']
+    filterset_fields = ['grade']
+
+
+class PersonalFileView(ModelViewSet):
+    serializer_class = GradeSerializer
+    queryset = Annual.objects.all()
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['profile', 'grade']
+    ordering_fields = ['profile', 'grade']
+    filterset_fields = ['profile', 'grade']
+
+
+
